50a326bb571b31dbf4806cadf296c104216e5cff
McDonald <emdeefx@gmail.com>
2020-07-25 17:17:49 +0100

    Made some changes to the .js file, effects causes the image to change when clicked.

--- testsites/scripts/main.js
+++ testsites/scripts/main.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute ('src');
+    if(mySrc === 'images/beerus_ultra-instinct.jpg') {
+        myImage.setAttribute ('src','images/ultra_instinct.jpg')
+    } else {
+        myImage.setAttribute ('src','images/beerus_ultra-instinct.jpg')
+    }
+}
